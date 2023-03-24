926058dd21e2359551d5e7845cfb3936280b008d
MrVendys <vasik.ptak@gmail.com>
2023-03-24 12:34:08 +0100

    neco 2

--- vrhcaby.py
+++ vrhcaby.py
@@ -1,1 +1,9 @@
 print("Hello world")
+#git config user.email "vasik.ptak@gmail.com"
+#git config user.name "MrVendys"
+#git add . -> přidá všechny soubory od Gitu
+#git commit -m "přidal jsem soubor vrhcaby"
+#git push
+#git pull
+
+
