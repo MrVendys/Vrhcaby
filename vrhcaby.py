6988ae83b5c1c7fb759d516446fe0b66ccb32028
vendys v <vasik.ptak@gmail.com>
2023-05-03 20:55:07 +0200

    Vytvoření základních tříd

--- vrhcaby.py
+++ vrhcaby.py
@@ -1,13 +1,34 @@
-print("Hello world")
-
-def f(x):
-  return x
-
-#git config user.email "vasik.ptak@gmail.com"
-#git config user.name "MrVendys"
-#git add . -> přidá všechny soubory od Gitu
-#git commit -m "přidal jsem soubor vrhcaby"
-#git push
-#git pull
+# git config user.email "vasik.ptak@gmail.com"
+# git config user.name "MrVendys"
+# git add . -> přidá všechny soubory od Gitu
+# git commit -m "přidal jsem soubor vrhcaby"
+# git push
+# git pull
 
 
+class player:
+    def __init__(self, color: str):
+        self.listOfPieces = [piece(color) for _ in range(16)]
+
+
+class board:
+    def __init__(self):
+        self.boardList = []
+        for i in range(24):
+            self.boardList.append([None for _ in range(5)])
+
+
+class piece:
+    def __init__(self, pieceColor: str):
+        self.pieceColor = pieceColor
+
+
+class dice:
+    def __init__(self, numberOfSides: int):
+        self.numberOfSides = numberOfSides
+
+
+gameBoard = board()
+Venca = player("black")
+Stepan = player("white")
+print(Venca.listOfPieces[0].pieceColor)
