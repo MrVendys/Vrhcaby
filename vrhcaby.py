90da0023c24fdafb64df4d1cf1da1a991e0a9c88
MrVendys <vasik.ptak@gmail.com>
2023-09-10 15:14:18 +0200

    problemy s AI move

--- vrhcaby.py
+++ vrhcaby.py
@@ -9,6 +9,7 @@
 import time
 import pygame
 import pygame.gfxdraw
+from dice import Dice
 from gameBoard import GameBoard
 from piece import Piece
 from player import Player
@@ -36,8 +37,8 @@ def main():
     global highlightedPiece
     highlightedSpikes = []
     highlightedPiece = None
-    global playerTurn
-    playerTurn = True
+    global playerOnTurn
+    playerOnTurn = playerW
     clock = pygame.time.Clock()
     run = True
     global AIAlreadyPlay
@@ -49,12 +50,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if(playerTurn):
+            if(playerOnTurn == playerW):
                 #Hraje hráč
-                print("Hraje hráč")
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     click_x, click_y = event.pos
-                    print(playerTurn)
                     #Klik myší
                     for spike in highlightedSpikes:
                         if(pygame.draw.polygon(WIN, spike.color, spike.position).collidepoint(click_x, click_y) == True and spike.isHighlighted == True):
@@ -66,43 +65,45 @@ def main():
                     if(len(playerW.playerBar.listOfPieces)>0):
                         # Kliknutí na hrací kámen na baru
                         if(pygame.draw.rect(WIN,(0,0,0),playerW.playerBar.position,5).collidepoint(click_x, click_y)):
-                            managePiece(playerW.playerBar.listOfPieces[0])
+                            highlightPiece(playerW.playerBar.listOfPieces[0])
                     else:         
                         # Kliknutí na hrací kámen      
                         for piece in playerW.listOfPieces:
                             if (pygame.draw.circle(WIN, piece.color, piece.positions, piece.radius).collidepoint(click_x, click_y)):
-                                managePiece(piece)
+                                highlightPiece(piece)
                                 break
             else:
                 #Hraje počítač
                 #board.drawAIplays(WIN)
                 if(AIAlreadyPlay == False):
                     AIPlay()
-                    time.sleep(3)
                     draw()
 def AIPlay():
     global AIAlreadyPlay
+    global highlightedPiece
     AIAlreadyPlay = True
     #když vyhodím jeden, tak ten můj se považuje za černý
     #posune figurku do domečku
     #vyhodí hráčovu figurku
     #posune se svojí figurkou
-    index = 0
     succes = False
     while(succes == False):
-        piece = playerB.listOfPieces[index].allSpikes[-1].listOfPieces[-1]
+        piece = playerB.listOfPieces[0].allSpikes[-1].listOfPieces[-1]
 
         if(len(playerB.playerBar.listOfPieces) > 0):
-            managePiece(playerB.playerBar.listOfPieces[0])
+            highlightPiece(playerB.playerBar.listOfPieces[0])
         else:
-            managePiece(piece)
-        print("id spiku počátku: ",piece.allSpikes.index(piece.allSpikes[-1]))
+            highlightPiece(piece)
+        print("id spiku počátku: ",highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1]))
         print("kostka: ",board.dice1.numberOnDice)
-        print("id spkiku",piece.allSpikes.index(piece.allSpikes[-1])+board.dice1.numberOnDice)
-        succes = lookOnSpike(board.boardList[piece.allSpikes.index(piece.allSpikes[-1]) + board.dice1.numberOnDice])
+        print("id spkiku",highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1])+board.dice1.numberOnDice)
+        succes = lookOnSpike(board.boardList[highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1]) + board.dice1.numberOnDice])
         print("Succes je: ",succes)
         if(succes == False):
-            succes = lookOnSpike(board.boardList[piece.allSpikes.index(piece.allSpikes[-1]) + board.dice2.numberOnDice])
+            succes = lookOnSpike(board.boardList[highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1]) + board.dice2.numberOnDice])
+            print("id spiku počátku: ",highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1]))               
+            print("kostka: ",board.dice2.numberOnDice)
+            print("id spkiku",highlightedPiece.allSpikes.index(highlightedPiece.allSpikes[-1])+board.dice2.numberOnDice)
             if(succes == False):
                 pass
             else:
@@ -118,55 +119,65 @@ def lookOnSpike(spike: Spike):
     if(spike.listOfPieces[0].color == (0,0,0) or (spike.listOfPieces[0].color == (255,255,255) and len(spike.listOfPieces)<2)):
         manageSpike(spike)
         return True
+    elif(spike.listOfPieces[0].color == (255,255,255 and len(spike.listOfPieces) > 1)):
+        return False
     else:
         return False
 def nextTurn():
     print("Next turn")
     """
     if(board.dice1.numberOnDice == 0 and board.dice2.numberOnDice == 0):
-        global playerTurn
-        if(playerTurn == True):
-            playerTurn = False
+        global playerOnTurn
+        global playerW
+        global playerB
+        if(playerOnTurn == playerW):
+            playerOnTurn = playerB
         else: 
-            playerTurn = True
+            playerOnTurn = playerW
         board.throwDices() 
     """
-    global playerTurn
-    if(playerTurn == True):
-        playerTurn = False
+    
+    global playerOnTurn
+    global playerW
+    global playerB
+    print(f"{playerOnTurn} má na baru: ",len(playerOnTurn.playerBar.listOfPieces))
+    
+    if(playerOnTurn == playerW):
+        playerOnTurn = playerB
     else: 
-        playerTurn = True
+        playerOnTurn = playerW
     #board.throwDices() 
     
-def managePiece(piece: Piece):
+def highlightPiece(piece: Piece):
     global highlightedPiece
+    global playerW
     if(highlightedPiece != None):
         removeHighlight()
-    if(playerTurn):
+    if(playerOnTurn == playerW):
         highlight(piece.allSpikes[-1].listOfPieces[-1])
         draw()
     else:
         highlightedPiece = piece
 def manageSpike(spike):
     #Spike je prázdný
-   if(len(spike.listOfPieces)==0):
-       movePiece(spike) 
-       #Na spike je jeden kámen
-   elif(len(spike.listOfPieces)==1):
-       if(spike.listOfPieces[0].color == (0,0,0)):
-           popedPiece = spike.removePiece()
-           if(popedPiece != None):
-               #kámen se vyhodí a dá se na bar
-               playerB.playerBar.addPiece(popedPiece)
-       movePiece(spike)
-       #Na spike je víc kamenů
-   elif(len(spike.listOfPieces)>1 and len(spike.listOfPieces) < 5 and spike.listOfPieces[0].color == (255,255,255)):
-       movePiece(spike)
-   elif(not(playerTurn)):
-       return False
-     
-   removeHighlight()
-   draw()
+    if(len(spike.listOfPieces)==0):
+        movePiece(spike) 
+        #Na spike je jeden kámen
+    elif(len(spike.listOfPieces)==1):
+            if(spike.listOfPieces[0].color != highlightedPiece.color):
+                #kámen se vyhodí a dá se na bar
+                player = playerW if playerOnTurn == playerB else playerB
+                poppedPiece = spike.removePiece()
+                player.playerBar.addPiece(poppedPiece)
+                poppedPiece.allSpikes.append( player.playerBar) 
+            movePiece(spike)
+        #Na spike je víc kamenu
+    elif(len(spike.listOfPieces)>1 and len(spike.listOfPieces) < 5 and spike.listOfPieces[0].color == (255,255,255)):
+        movePiece(spike)
+    elif(playerOnTurn == playerB):
+        return False
+    removeHighlight()
+    draw()
 def draw():
     WIN.fill(WIN_COLOR)
     board.drawGameBoard(WIN)
@@ -176,15 +187,12 @@ def movePiece(spike):
     global highlightedPiece
     highlightedPiece.allSpikes[-1].removePiece()
     difference = abs(spike.id - highlightedPiece.allSpikes[-1].id)
-    #useDice(difference)
+    #useDice(board.dice1 if difference == board.dice1.numberOnDice else board.dice2)
     spike.addPiece(highlightedPiece)
     highlightedPiece.addSpike(spike)
     nextTurn()  
-def useDice(number: int):
-    if(number == board.dice1.numberOnDice):
-        board.dice1.numberOnDice = 0
-    elif (number == board.dice2.numberOnDice):
-        board.dice2.numberOnDice = 0
+def useDice(dice: Dice):
+        dice.numberOnDice = 0
 def removeHighlight():
     global highlightedSpikes
     global highlightedPiece
